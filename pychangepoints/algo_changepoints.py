e799fa75266b7461e16f765c460364ed17c60aa0
brunedv <vianney.bruned@gmail.com>
2018-12-29 22:56:17 +0100

    segneigh

--- pychangepoints/algo_changepoints.py
+++ pychangepoints/algo_changepoints.py
@@ -1,5 +1,5 @@
 import numpy as np
-from .cython_pelt import cpelt, cbin_seg, cnp_pelt
+from .cython_pelt import cpelt, cbin_seg, cnp_pelt, cseg_neigh
 from .multiple_dim import cbin_seg_multiple, cpelt_multiple
 
 def nonpamametric_ed_sumstat( data, K=10):
@@ -36,7 +36,16 @@ def np_pelt(data, pen_, minseg=10,nquantiles=10, method="mbic_nonparametric_ed")
     nquantiles_= int(2*np.log(size_ts))
     sumstat = nonpamametric_ed_sumstat(data,nquantiles_)
     return  cnp_pelt(sumstat, pen_,minseg,size_ts-1,nquantiles_,method_)
+def segneigh( data, Q, method):
+    times_series =data.values
+    mean = np.mean(times_series)
+    size_ts = times_series.shape[0]
+    stats_ts = np.zeros((size_ts,3))
+    stats_ts[:,0] = times_series.cumsum()
+    stats_ts[:,1] = (times_series**2).cumsum()
+    stats_ts[:,2] = ((times_series-mean)**2).cumsum()
 
+    return cseg_neigh( stats_ts, Q, method)
 def binseg(data, Q, minseg, method):
     times_series =data.values
     mean = np.mean(times_series)
--- pychangepoints/pelt_cython.pyx
+++ pychangepoints/pelt_cython.pyx
@@ -235,12 +235,7 @@ def cbin_seg( np.ndarray[DTYPE_t, ndim=2] sumstat, ITYPE_t Q, ITYPE_t minseglen,
         order_vec(tau_,q+3)
 
     return np.array(cptsout)
-def optimal_partionning(np.ndarray[DTYPE_t, ndim=2] sumstat, int Q, int minseglen):
-    cdef int n = sumstat.shape[0]-1
-    cdef double [:]  like_Q =np.zeros(Q)
-    cdef int cp = np.zeros(Q)
-    cdef int all_seg = np.zeros(2*20)
-    return Q
+
 def cnp_pelt( np.ndarray[DTYPE_t, ndim=2] sumstat, double pen, int minseglen, int n, int nquantiles, str method):
     #cdef int n = sumstat.shape[0] - 1
     cdef int error = 0
@@ -317,3 +312,67 @@ def cnp_pelt( np.ndarray[DTYPE_t, ndim=2] sumstat, double pen, int minseglen, in
         last=lastchangecpts[last]
         ncpts+=1
     return np.array(cptsout)[0:ncpts],ncpts
+
+def cseg_neigh( np.ndarray[DTYPE_t, ndim=2] sumstat, ITYPE_t Q, str method):
+    cdef ITYPE_t n=sumstat.shape[0]
+    cdef np.ndarray[DTYPE_t, ndim=2] all_seg=np.zeros((n,n))
+    cdef int i, j, q
+    cdef DTYPE_t s
+
+    if method =="mll_mean":
+        current_cost = mll_mean
+    elif method == "mll_var":
+        current_cost = mll_var
+    elif method == "mll_meanvar":
+        current_cost = mll_meanvar
+    elif method == "mll_meanvar_exp":
+        current_cost = mll_meanvar_exp
+    elif method == "mll_meanvar_poisson":
+        current_cost = mll_meanvar_poisson
+    elif method == "mbic_var":
+        current_cost = mbic_var
+    elif method == "mbic_meanvar":
+        current_cost = mbic_meanvar
+    elif method == "mbic_mean":
+        current_cost = mbic_mean
+    elif method == "mbic_meanvar_exp":
+        current_cost = mbic_meanvar_exp
+    elif method == "mbic_meanvar_poisson":
+        current_cost = mbic_meanvar_poisson
+    else:
+        current_cost = mll_meanvar
+    for i in range(0,n) :
+        for j in range(i,n):
+            all_seg[i,j] = -0.5*current_cost(sumstat[j,0]-sumstat[i,0],sumstat[j,1]-sumstat[i,1],sumstat[j,2]-sumstat[i,2] , j - i+1)
+    cdef np.ndarray[DTYPE_t, ndim=2] like_q = np.zeros((Q,n))
+    like_q[0,:] = all_seg[0,:]
+    cdef  np.ndarray[ITYPE_t, ndim=2] cp = np.zeros((Q,n),dtype=np.int64)
+    cdef DTYPE_t max_out =-np.inf
+    cdef ITYPE_t max_which
+    cdef np.ndarray[DTYPE_t, ndim=1] like
+
+    for q in range(1,Q):
+        for j in range(q,n):
+            like = like_q[q-1,(q-1):(j-1)]+all_seg[q:j,j]
+            max_which = 0
+            max_out =-np.inf
+            for i in range(0,j-q):
+                if  max_out <=like[i]:
+                    max_out = like[i]
+                    max_which = i
+            #print(max_out,max_which)
+
+            like_q[q,j] = max_out
+            cp[q,j] = max_which + q
+    cdef  np.ndarray[ITYPE_t, ndim=2] cps_Q = np.zeros((Q,Q),dtype=np.int64)
+    for q in range(1,Q):
+        cps_Q[q,0]=cp[q,n-1]
+        for i in range(0,q):
+            cps_Q[q,i+1]=cp[q-i-1,cps_Q[q,i]]
+  
+    criterium = -2 * like_q[:,n-2]
+    op_cps = np.argmin(criterium)
+    cpts = np.sort(cps_Q[op_cps, :][cps_Q[op_cps, :] > 0])
+  
+    return cpts
+
